e034fb5f8564f8c323333a99055b75e80073b183
miaowization <aigul.g.r@mail.ru>
2018-06-04 18:57:34 +0300

    Параметризированы тесты для создания групп и контактов

--- test/add_contact.py
+++ test/add_contact.py
@@ -1,30 +1,65 @@
 from model.contact import Contact
+import random
+import pytest
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix+"".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_digits(maxlen):
+    return "".join([random.choice(string.digits) for i in range(random.randrange(maxlen))])
 
 
-def test_add_contact(app):
+testdata = [Contact(first_name=first_name, middle_name=middle_name, last_name=last_name,
+                    home_telephone=home_telephone, mobile_telephone=mobile_telephone,
+                    work_telephone=work_telephone, email1=email1
+                    # title=title, company=company, company_address=company_address,
+                    #   home_telephone=home_telephone, mobile_telephone=mobile_telephone, work_telephone=work_telephone,
+                    #   fax=fax, email1=email1, email2=email2, email3=email3, homepage=homepage, address=address,
+                    #   secondary_telephone=secondary_telephone, notes=notes
+                    )
+            for first_name in ["", random_string("first_name", 10)]
+            for middle_name in ["", random_string("middle_name", 10)]
+            for last_name in ["", random_string("last_name", 10)]
+            # for nickname in ["", random_string("nickname", 10)]
+            # nickname=nickname
+            # for title in ["", random_string("title", 10)]
+            # for company in ["", random_string("company", 5)]
+            # for company_address in ["", random_string("company_address", 10)]
+            for home_telephone in ["", random_digits(10)]
+            for mobile_telephone in ["", random_digits(10)]
+            for work_telephone in ["", random_digits(10)]
+            # for fax in ["", random_digits(10)]
+            for email1 in ["", random_string("title", 10)]
+            # for email2 in ["", random_string("title", 10)]
+            # for email3 in ["", random_string("title", 10)]
+            # for homepage in ["", random_string("homepage", 10)]
+            # for address in ["", random_string("address", 20)]
+            # for secondary_telephone in ["", random_digits(10)]
+            # for notes in ["", random_string("notes", 20)]
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(first_name="Contact", middle_name="Contactovich", last_name="Contactov",
-                      nickname="Contactik", title="Manager", company="GDC", company_address="Prospect Pobedy",
-                      home_telephone="7894584", mobile_telephone="8448558", work_telephone="448454", fax="1448",
-                      email1="contact@mail.com", email2="contactik@ya.ru", email3="cont@gmail.com",
-                      homepage="contact.ya.ru", address="Lenina 22", secondary_telephone="334475", notes="Do not write here")
-
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
 
 
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    empty_contact = Contact(first_name="", middle_name="", last_name="", nickname="",
-                            title="", company="",
-                            company_address="", home_telephone="",
-                            mobile_telephone="", work_telephone="", fax="",
-                            email1="", email2="", email3="",
-                            homepage="", address="", secondary_telephone="", notes="")
-    app.contact.create(empty_contact)
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
+# def test_add_empty_contact(app):
+#     old_contacts = app.contact.get_contact_list()
+#     empty_contact = Contact(first_name="", middle_name="", last_name="", nickname="",
+#                             title="", company="",
+#                             company_address="", home_telephone="",
+#                             mobile_telephone="", work_telephone="", fax="",
+#                             email1="", email2="", email3="",
+#                             homepage="", address="", secondary_telephone="", notes="")
+#     app.contact.create(empty_contact)
+#     new_contacts = app.contact.get_contact_list()
+#     assert len(old_contacts) + 1 == len(new_contacts)
 
 
 
--- test/delete_group.py
+++ test/delete_group.py
@@ -5,7 +5,7 @@ def test_delete_first_group(app):
     old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.delete_first_group()
+    app.group.delete_group_by_index(0)
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
     old_groups[0:1] = []
