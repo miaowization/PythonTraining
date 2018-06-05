3d35d9d29ee282a82af996ee8274128a92bcc241
miaowization <aigul.g.r@mail.ru>
2018-06-05 17:24:49 +0300

    Реализованы опции командной строки для браузера и адреса веб-приложения

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,15 +1,19 @@
 import re
-
+import random
 
 def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contacts = app.contact.get_contact_list()
+    index = random.randrange(len((contacts)))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == app.contact.merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contacts = app.contact.get_contact_list()
+    index = random.randrange(len((contacts)))
+    contact_from_view_page = app.contact.get_contact_info_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_view_page.home_telephone == contact_from_edit_page.home_telephone
     assert contact_from_view_page.work_telephone == contact_from_edit_page.work_telephone
     assert contact_from_view_page.mobile_telephone == contact_from_edit_page.mobile_telephone
